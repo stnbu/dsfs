76a7396071f5978514e1018443813971d1e412cf
Mike Burr <mb@unintuitive.org>
2021-08-06 14:28:42 -0600

    Now with bind address specification CLI option.

--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,44 @@
 /// It re-uses existing tools and is dead-simple as a "feature".
 ///
 /// ```
-/// dsfs ./   # serves current directory at localhost:8000
-/// dsfs      # serves current directory at localhost:8000
-/// dsfs /tmp # serves /tmp/ localhost:8000
+/// dsfs                         # serve current directory at 0.0.0.0:8000
+/// dsfs --address 1.2.3.4 ./www # serve ./www at 1.2.3.4:8000
+/// dsfs --address 1.2.3.4:9000  # serve current directory at 1.2.3.4:9000
+/// dsfs ./www                   # serve ./www at 0.0.0.0:8000
 /// ```
-
-use std::env;
+use clap::{App, Arg};
+use rocket::config::{Config, Environment};
 use rocket_contrib::serve::StaticFiles;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = match args.len() {
-        1 => "./",
-        2 => &args[1],
-        _ => panic!("Usage: {} [path]\n\tServe content of `path` directiory at localhost:8000",
-                    args[0]),
-    };
-    rocket::ignite()
+    let matches = App::new("Dead Simple File Server")
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .arg(
+            Arg::with_name("address")
+                .short("a")
+                .long("address")
+                .value_name("IPV4_ADDR")
+                .help("An IPv4 address of the form a.b.c.d:1234")
+                .takes_value(true),
+        )
+        .arg(Arg::with_name("path").index(1))
+        .get_matches();
+    let address = matches.value_of("address").unwrap_or("0.0.0.0:8000");
+    let path = matches.value_of("path").unwrap_or(".");
+
+    let mut split = address.split(":");
+    let ip = split.next().unwrap_or("0.0.0.0");
+    let port = split.next().unwrap_or("8000");
+    let port = port.parse().expect("not a number");
+
+    let config = Config::build(Environment::Staging)
+        .address(ip)
+        .port(port)
+        .finalize()
+        .expect("Unable to finalize configuration.");
+
+    rocket::custom(config)
         .mount("/", StaticFiles::from(path))
         .launch();
 }
-
